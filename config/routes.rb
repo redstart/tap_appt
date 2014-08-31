5e5999eb10e1179ebef00c2fac96c09494c153ef
redstartme@gmail.com <redstartme@gmail.com>
2014-08-31 23:56:34 +0400

    Devise implemented for user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TapAppt::Application.routes.draw do
+  
+  devise_for :users
   root to: "welcome#index"
+  resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
